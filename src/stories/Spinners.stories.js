f9f1dddd9086608433ea137a0093d5476b6723ce
daniel-kao <396007476@qq.com>
2020-10-08 09:38:17 +0800

    storybook
    Signed-off-by: daniel-kao <396007476@qq.com>

---
+++ src/stories/Spinners.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Leaf, Pulse, Rolling } from '..'
+
+export default {
+  title: 'Example/Leaf'
+}
+
+export const LeafStory = () => <Leaf />
+
+export const PulseStory = () => <Pulse />
+
+export const RollingStory = () => <Rolling />
