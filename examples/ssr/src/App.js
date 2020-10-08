1157fa3b9f9b6d0d12fbcafb91b623ea3c0220df
daniel-kao <396007476@qq.com>
2020-10-08 09:37:04 +0800

    examples modified
    Signed-off-by: daniel-kao <396007476@qq.com>

---
+++ examples/ssr/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Leaf, Pulse, Rolling } from 'daniel-fancy-spinners'
+
+const App = () => (
+  <>
+    <Leaf />
+    <Pulse />
+    <Rolling />
+  </>
+)
+
+export default App
